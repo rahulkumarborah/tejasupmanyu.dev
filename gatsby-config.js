63c77462db7b8030bdbcc88fb3526e180fb37e3c
Tejas-CureFit <tejas.upmanyu@v.curefit.com>
2020-04-25 12:33:08 +0530

    Added you probably don't need redux and google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-164567288-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
